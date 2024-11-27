eea99e20f17eb8c519782552ce6fa5f7054e341d
alanw0ng <alanjw8@gmail.com>
2024-11-27 01:11:29 -0800

    t1

---
+++ use_ParsingResults.sh
@@ -0,0 +1,307 @@
+# For Build v01.06.23
+
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_ota_self_results_v01_06_23.txt MG22332A2US0019 qa1 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_ota_power_results_v01_06_23.txt guo_sn_14_o2 qa6 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_ota_int_upgrade_results_v01_06_23.txt MG22332A2US0019 home_O2 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_ota_ext_upgrade_results_v01_06_23.txt MG22332A2US0019 home_O2 rls-v01.06.23-562210a
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_idle_op_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_lip_op_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_hip_op_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_cooldown_op_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_fluff_op_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+
+
+
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_idle_mass_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_lip_mass_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_hip_mass_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_cooldown_mass_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_dgo_fluff_mass_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+
+
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_eeprom_stress_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_pairing_results_v01_06_23.txt MG22332A2US0019 home_O2 rls-v01.06.23-562210a
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_start_via_bin_B_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_start_via_bin_A_stress_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_child_lock_B_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_child_lock_A_stress_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+
+
+
+python3 Use_ParsingResults.py rls-v01.06.23-562210a/O2_boot_cycle_A_stress_results_v01_06_23.txt MG22332A2US0019 qa9 rls-v01.06.23-562210a
+sleep 3
+
+
+# 1. update to:  rls-v01.06.23-562210a
+# 2. update to:  v01_06_23.txt
+# 3. update SN:
+# 4. update svr: 
+#########################################################################################
+
+
+
+# For Build v01.05.05
+
+#python3 Use_ParsingResults.py rls-v01.05.05-19dc16c_2/O2_dgo_idle_op_results_v01_05_05.txt MG22332A2US0019 qa7 dbg-v01.05.05-19dc16c
+# sleep 3
+#python3 Use_ParsingResults.py rls-v01.05.05-19dc16c_2/O2_ota_self_results_v01_05_05.txt MG22332A2US0019 qa7 dbg-v01.05.05-19dc16c
+
+
+
+
+# For Build v01.06.19
+
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_ota_self_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_ota_power_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_ota_int_upgrade_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_ota_ext_upgrade_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_idle_op_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_lip_op_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_hip_op_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_cooldown_op_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_fluff_op_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+
+
+
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_idle_mass_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_lip_mass_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_hip_mass_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_cooldown_mass_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_dgo_fluff_mass_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+# sleep 3
+
+
+python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_eeprom_stress_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_pairing_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_start_via_bin_B_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_start_via_bin_A_stress_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_child_lock_B_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+sleep 3
+python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_child_lock_A_stress_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+sleep 3
+
+
+
+python3 Use_ParsingResults.py rls-v01.06.19-8cafe27/O2_boot_cycle_A_stress_results_v01_06_19.txt MG22332A2US0019 qa3 rls-v01.06.19-8cafe27
+sleep 3
+
+
+
+
+
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+# For Build v01.06.15
+
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_ota_self_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_ota_power_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_ota_int_upgrade_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_ota_ext_upgrade_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_idle_op_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_lip_op_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_hip_op_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_cooldown_op_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_fluff_op_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_idle_mass_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_lip_mass_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_hip_mass_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_cooldown_mass_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_dgo_fluff_mass_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_eeprom_stress_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_pairing_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_start_via_bin_B_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_start_via_bin_A_stress_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_child_lock_B_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.06.15-16daade/O2_child_lock_A_stress_results_v01_06_15.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# For build: v01.05.08 O2
+
+# python3 Use_ParsingResults.py 'rls-v01.05.08-ba5f554/O2_ota_self_results_v01_05_08.txt' MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_ota_power_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_ota_int_upgrade_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_ota_ext_upgrade_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+
+## ============================================================
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_idle_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_lip_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_hip_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_cooldown_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_fluff_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# #================================================================
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_idle_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_lip_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_hip_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_cooldown_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_dgo_fluff_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# # #================================================================
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_eeprom_stress_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_pairing_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_start_via_bin_B_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_start_via_bin_A_stress_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_child_lock_B_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O2_child_lock_A_stress_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# For build: v01.05.08 for O1
+
+# python3 Use_ParsingResults.py 'rls-v01.05.08-ba5f554/O1_ota_self_results_v01_05_08.txt' MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_ota_power_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_ota_prod_upgrade_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_ota_int_upgrade_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_ota_ext_upgrade_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# ## ============================================================
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_idle_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_lip_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_hip_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_cooldown_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_fluff_op_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# # #================================================================
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_idle_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_lip_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_hip_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_cooldown_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_dgo_fluff_mass_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# # # # #================================================================
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_eeprom_stress_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_pairing_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_start_via_bin_B_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_start_via_bin_A_stress_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_child_lock_B_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_child_lock_A_stress_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_lid_open_close_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+# python3 Use_ParsingResults.py rls-v01.05.08-ba5f554/O1_safety_test_results_v01_05_08.txt MG22332A2US0019 qa3 rls-v01.05.08-ba5f554
+# sleep 3
+
+
