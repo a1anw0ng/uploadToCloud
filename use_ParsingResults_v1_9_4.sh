8dc578401aed905e9758a251f166338a2febfdaa
alanw0ng <alanjw8@gmail.com>
2024-11-30 22:50:48 -0800

    v1_9_*

---
+++ use_ParsingResults_v1_9_4.sh
@@ -0,0 +1,256 @@
+# For Build v01.09.00
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_ota_self_results_v01_09_04.txt MG22332A2US0019 qa7 rls-v01.09.04-4008370 qa7
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_ota_power_results_v01_09_04.txt guo_sn_14_o2 qa6 rls-v01.09.04-4008370 qa6_O2
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_ota_int_upgrade_results_v01_09_04.txt MG22332A2US0019 qa7 rls-v01.09.04-4008370 qa7
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_ota_ext_upgrade_results_v01_09_04.txt MG22332A2US0019 qa7 rls-v01.09.04-4008370 qa7
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_ota_prod_upgrade_results_v01_09_04.txt MG22332A2US0019 qa7 rls-v01.09.04-4008370 qa7
+sleep 3
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_idle_op_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_lip_op_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_hip_op_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_cooldown_op_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_fluff_op_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_idle_mass_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_lip_mass_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_hip_mass_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_cooldown_mass_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_dgo_fluff_mass_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_eeprom_stress_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_pairing_results_v01_09_04.txt MG22335A2US0024 home_O2 rls-v01.09.04-4008370 home_O2
+sleep 3 
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_start_via_bin_B_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_start_via_bin_A_stress_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_child_lock_B_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_child_lock_A_stress_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_safety_test_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_boot_cycle_A_stress_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_smoke_pairing_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_smoke_dgo_inactive_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_smoke_test_lid_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_add_mass_hip_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_reset_hip_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_sht40_calibration_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_sht40_cal_skip_when_hot_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_start_stop_no_vacation_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_jam_error_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_jam_inactive_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_empty_bucket_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_heat_warning_status_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_dgo_hip_moisture_food_add_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_cooldown_temp_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_heat_led_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_premature_mass_no_reset_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_locked_when_hot_shadows_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_vacation_clean_mode_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_vacation_eco_mode_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_vacation_add_mass_cooldown_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_vacation_reset_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_grinder_soft_stall_retry_same_dir_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_grinder_state_lid_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_grinder_high_temp_jam_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_grinder_jam_auto_retry_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_fluff_not_run_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_always_locked_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_always_unlocked_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_bucket_fullness_detection_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_locked_when_running_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_fr_heat_warning_status_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_delta_mr_mass_bucket_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_resume_from_lip_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_set_dgo_start_time_during_hip_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_unprocessed_mass_zero_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_mass_add_lid_open_close_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_mass_no_change_reset_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_locked_when_running_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_lastDgoCycle_energy_shadows_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_cycleEndTime_lid_open_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_lip_skipped_hot_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_hip_extension_lid_open_close_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_start_disconnected_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_resume_disconnected_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_heat_led_interruptions_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_vacation_after_empty_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_resume_cooldown_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_vacation_ota_pre_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_vacation_ota_post_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_bucket_crash_ota_pre_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+python3 Use_ParsingResults.py rls-v01.09.04-4008370/O2_bat_bucket_crash_ota_post_results_v01_09_04.txt MG32401A2US100204 qa9 rls-v01.09.04-4008370 qa9
+sleep 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Template:
+# python3 Use_ParsingResults.py <buildN folder>/<each results file> <SN> <serverN> <buildN> <runner>
+
+# 1. update to:  rls-v01.07.00-37e01a1
+# 2. update to:  v01_06_23.txt
+# 3. update SN:
+# 4. update svr: 
+# 5. update runner:
+# ########################################################################################
+
